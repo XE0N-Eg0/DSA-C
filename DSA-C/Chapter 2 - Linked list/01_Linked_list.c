ee90da08fabe5db70353df6dea0deeaff41f5859
XE0N_eg0 <work.raghu101@gmail.com>
2024-11-28 03:33:47 +0530

    Update 01_Linked_list.c

--- DSA-C/Chapter 2 - Linked list/01_Linked_list.c
+++ DSA-C/Chapter 2 - Linked list/01_Linked_list.c
@@ -54,6 +54,50 @@ int main(int argc, char const *argv[])
     head_1 ->next = first_1;
     first_1 ->next = second_1;
     second_1 ->next = third_1;
+    third_1 ->next = NULL;
+
+    // creation and linking of fx_2
+
+    struct node *head_2 = NULL;
+    struct node *prev_2 = NULL;
+    int num, value;
+
+    printf("Enter the amount of nodes you want: ");
+    scanf("%d", &num);
+
+    for (int i = 0; i < num; i++) {
+        printf("Enter the value of element %d: ", i + 1);
+        scanf("%d", &value);
+        struct node *newnode = create_node_2(value, prev_2);
+        if (head_2 == NULL) {
+            head_2 = newnode;
+        }
+        prev_2 = newnode;
+    }
+    
+    // display
+    int choice;
+    printf("choose the option to display\n");
+    printf("1. LL - 1 (manual)\n");
+    printf("2. LL - 2 (semi-manual)\n");
+    printf("3. LL - 2 (automatic)\n");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+    case 1:
+        display(head);
+        break;
+    case 2:
+        display(head_1);
+        break;
+    case 3:
+        display(head_2);
+    default:
+        break;
+    }
+
+    
+
 
 
     return 0;
@@ -65,6 +109,7 @@ void display(struct node * head){
         printf("%d -> ", head->data);
         head = head -> next;
     }
+    printf("NULL\n");
     
 }
 
@@ -72,5 +117,15 @@ struct node * create_node_1(int value){
     struct node * new_node = (struct node *)malloc(sizeof(struct node));
     new_node -> data = value;
     new_node -> next = NULL;
+    return new_node;
+}
+
+struct node * create_node_2(int value, struct node * prev_2){
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = value;
+    new_node->next = NULL;
+    if (prev_2 != NULL) {
+        prev_2->next = new_node;
+    }
     return new_node;
 }
