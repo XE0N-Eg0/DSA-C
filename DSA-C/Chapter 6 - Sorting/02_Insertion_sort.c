7d6f3c2deea98ee50372d69dd7e19f17b8263c0d
XE0N_eg0 <work.raghu101@gmail.com>
2025-01-01 21:11:41 +0530

    Added Insertion Sort and minor Changes

--- DSA-C/Chapter 6 - Sorting/02_Insertion_sort.c
+++ DSA-C/Chapter 6 - Sorting/02_Insertion_sort.c
@@ -2,7 +2,7 @@
 
 static int array[] = {10, 1, 9, 2, 8, 3, 7, 4, 6, 5}; // Prime Global Array [cant be const]
 
-void Bubble_Sort(int array[], int size);
+void Insertion_Sort(int array[], int size);
 void swap(int *a, int *b);
 void Print_Array(int array[], int size);
 
@@ -11,31 +11,25 @@ int main(int argc, char const *argv[])
 {
     int size = sizeof(array) / sizeof(array[0]);
     Print_Array(array, size);
-    Bubble_Sort(array, size);
+    Insertion_Sort(array, size);
     Print_Array(array, size);
 
     return 0;
 }
 
-void Bubble_Sort(int array[], int size)
+void Insertion_Sort(int array[], int size)
 {
-    for (int i = 0; i < size - 1; i++)
+    for (int i = 1; i < size; i++)
     {
-        for (int j = 0; j < size - 1; j++)
+        int current = array[i];
+        int j = i - 1;
+        while (j >= 0 && array[j] > current) // This loop runs untill j is 0 and it keeps shifting elements to next
         {
-            if (array[j] > array[j + 1]) // if the element before is larger than next it gets swapped [ascending]
-            {
-                swap(&array[j], &array[j + 1]); // passed by reference ('&')
-            }
+            array[j + 1] = array[j];
+            j--;
         }
+        array[j + 1] = current; // inserts the element into correct position
     }
-}
-
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
 }
 
 void Print_Array(int array[], int size)
